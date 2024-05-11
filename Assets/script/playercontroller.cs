66c97c4f15487483ee64583754db1ac5f7606d33
Mustafa-Seyyit <148425312+Mustafa-Seyyit@users.noreply.github.com>
2024-05-11 14:36:29 +0300

    player animations & bisey

--- Assets/script/playercontroller.cs
+++ Assets/script/playercontroller.cs
@@ -7,13 +7,15 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public float movementspeed = 5f; //hareket hızı
-    public float rotationspeed = 10f;
+    public float rotationspeed = 10f; // oyuncu karakterinin dönüş hızını depolamak için bir değişken tanımladık 
     public Rigidbody rb;      //fizik işlemleri için rigidbody i tanımlama
+    private Animator animator; // animasyon işlemleri için animatörü tanımladık
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();   // aynı obje üzerinde rigidbody e ulaşmak için rigidbody i kullandık
+        animator = GetComponent<Animator>(); //animatöre ulaştık 
     }
 
     // Update is called once per frame
@@ -27,11 +29,17 @@ public class playercontroller : MonoBehaviour
 
         var movementDirection = new Vector3(horizontal, 0, vertical);
 
+        animator.SetBool("isrunning ", movementDirection !=Vector3.zero); 
+        
+       
+
         if(movementDirection== Vector3.zero)
         {
             Debug.Log(" su an input yok");
                 return;
         }
+
+
 
         // fiziksel olarak hizimiz yan eksenimiz ile hareket hizini carparak hareket ettirdik
 
