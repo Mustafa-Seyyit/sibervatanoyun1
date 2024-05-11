890eb8460938ca1fdc173ae3ef29db868585058d
Mustafa-Seyyit <148425312+Mustafa-Seyyit@users.noreply.github.com>
2024-05-11 13:14:19 +0300

    momentandrotation

---
+++ Assets/script/playercontroller.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class playercontroller : MonoBehaviour
+{
+    public float movementspeed = 5f; //hareket hızı
+    public float rotationspeed = 10f;
+    public Rigidbody rb;      //fizik işlemleri için rigidbody i tanımlama
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   // aynı obje üzerinde rigidbody e ulaşmak için rigidbody i kullandık
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); //yatay eksende giriş aldık 
+        var vertical = Input.GetAxis("Vertical");       // dikey eksende giriş aldık
+
+        //2 boyutlu olan yataty ve dikey eksenindeki girişleri 3 boyutlu ya çevirdik
+        //x ve y dekseni uzerindeki girişleri x ve z eksenine göre cevirdik 
+
+        var movementDirection = new Vector3(horizontal, 0, vertical);
+
+        if(movementDirection== Vector3.zero)
+        {
+            Debug.Log(" su an input yok");
+                return;
+        }
+
+        // fiziksel olarak hizimiz yan eksenimiz ile hareket hizini carparak hareket ettirdik
+
+        rb.velocity = movementDirection * movementspeed;
+
+
+        // fiziksel olarak hizimizi yan eksenimiz ile hareket hizini carparak hareket ettirdik 
+        var rotationDirection= Quaternion.LookRotation(movementDirection);
+
+        //karakterin rotation degerini katdettiğim rotation değerine smototh bir geçiş sağlar 
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationDirection, rotationspeed * Time.deltaTime);
+
+    }
+}
